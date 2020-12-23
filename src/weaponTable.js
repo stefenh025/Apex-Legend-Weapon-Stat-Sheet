662583b81ee4c551091824d17560926934ce7202
Stefen <stefenxh@gmail.com>
2020-12-23 00:09:35 -0800

    Object entries population option added

--- src/weaponTable.js
+++ src/weaponTable.js
@@ -21,7 +21,6 @@ export default class WeaponTable extends React.Component{
       let tempStatHolder = [];
       for (var ammo in this.props.statData){
         for (var gunName in this.props.statData[ammo]){
-          //Could use object entries here to skip a loop
           tempStatHolder = [];
           tempStatHolder.push(
             <td key={gunName}>{gunName}</td>
@@ -29,18 +28,20 @@ export default class WeaponTable extends React.Component{
           tempStatHolder.push(
             <td key={ammo + "" + gunName}>{ammo.charAt(0).toUpperCase() + ammo.slice(1)}</td>
           );
+        // Solution 1 w Object entries Causes a warning because it references a variable declared outside of the loop's scope
+        // Object.entries(this.props.statData[ammo][gunName]).forEach(key => {
+        //     typeof(key[1] === "object") ? 
+        //     tempHolder.push(<td key={key[0] + "" + key[1]}>{key[1] + ""}</td>) :
+        //     tempHolder.push(<td key={key[0] + "" + key[1]}>{key[1]}</td>);
+        // });
           for (var weaponStats in this.props.statData[ammo][gunName]){
-            if (typeof(this.props.statData[ammo][gunName][weaponStats]) === "object"){
+            (typeof(this.props.statData[ammo][gunName][weaponStats]) === "object") ?
               tempStatHolder.push(
-                <td key={gunName + "" + weaponStats}>{this.props.statData[ammo][gunName][weaponStats] + ""}</td>
-              )
-            }
-            else{
+                <td key={gunName + "" + weaponStats}>{this.props.statData[ammo][gunName][weaponStats] + ""}</td>) :
               tempStatHolder.push(
-                <td key={gunName + "" + weaponStats}>{this.props.statData[ammo][gunName][weaponStats]}</td>
-              )
-            }
+                <td key={gunName + "" + weaponStats}>{this.props.statData[ammo][gunName][weaponStats]}</td>)
           };
+          //Create a function to creat Table Row for readability
           this.createTableBody(tempStatHolder,tableBody, gunName);
         }
       }
